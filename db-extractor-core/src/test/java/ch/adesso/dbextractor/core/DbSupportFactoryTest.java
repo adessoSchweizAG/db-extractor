f443aea6477b2718b746db195a3c04709b19a25d
Daniel Mast <Daniel.Mast@adesso.ch>
2019-07-26 15:36:55 +0200

    add DbSupportFactory

---
+++ db-extractor-core/src/test/java/ch/adesso/dbextractor/core/DbSupportFactoryTest.java
@@ -0,0 +1,34 @@
+package ch.adesso.dbextractor.core;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import javax.sql.DataSource;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DbSupportFactoryTest {
+
+	@Mock
+	private DataSource dataSource;
+
+	@Test
+	public void driverClassNames() {
+		assertThat(DbSupportFactory.getDriverClassNames(), CoreMatchers.hasItems(
+				DbSupportHsqlDb.DRIVER_CLASS_NAME, DbSupportPostgres.DRIVER_CLASS_NAME));
+	}
+
+	@Test
+	public void createInstance() {
+		
+		for (String driverClassName : DbSupportFactory.getDriverClassNames()) {
+			assertNotNull(DbSupportFactory.createInstance(driverClassName, dataSource));
+		}
+	}
+
+}
