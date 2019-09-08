cdff1add78aca8f448c8dae686711c7907b92b52
Daniel Mast <Daniel.Mast@adesso.ch>
2019-09-08 18:47:45 +0200

    react-bootstrap

--- db-extractor-ui/src/main/js/frontend/src/App.js
+++ db-extractor-ui/src/main/js/frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import DataSourceConfig from './DataSourceConfig';
 
 function App() {
 	return <React.Fragment>
-		<h1>DB Extractor - React UI</h1>
+		<Navbar bg="dark" variant="dark">
+			<Navbar.Brand>DB Extractor - React UI</Navbar.Brand>
+		</Navbar>
 		<DataSourceConfig />
 	</React.Fragment>;
 }
