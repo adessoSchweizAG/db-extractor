58dbed7b20eea9c19b06a6c1c60924fd6a839f1b
Daniel Mast <Daniel.Mast@adesso.ch>
2020-03-21 17:50:10 +0100

    upgrade to NodeJS 12.16.1 (lts)

--- db-extractor-ui/src/main/js/frontend/src/App.js
+++ db-extractor-ui/src/main/js/frontend/src/App.js
@@ -9,35 +9,35 @@ import DataSourceConfig from './DataSourceConfig';
 import TableDataFilters from './TableDataFilters';
 
 function App() {
-	return <Provider store={store}>
-		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand>DB Extractor - React UI</Navbar.Brand>
-		</Navbar>
-		<Container fluid="true">
-			<Row>
-				<Col as="nav" className="sidebar bg-light" md="2">
-					<div className="sidebar-sticky">
-						<Accordion defaultActiveKey="datasourceConfig">
-							<Card>
-								<Accordion.Toggle as={Card.Header} eventKey="datasourceConfig">
-									Datasource
-								</Accordion.Toggle>
-								<Accordion.Collapse eventKey="datasourceConfig">
-									<Card.Body>
-										<DataSourceConfig />
-									</Card.Body>
-								</Accordion.Collapse>
-							</Card>
-						</Accordion>
-					</div>
-				</Col>
-				<Col md="10">
-					<TableDataFilters />
-				</Col>
-			</Row>
-		</Container>
-		
-	</Provider>;
+	return (
+		<Provider store={store}>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand>DB Extractor - React UI</Navbar.Brand>
+			</Navbar>
+			<Container fluid="true">
+				<Row>
+					<Col as="nav" className="sidebar bg-light" md="2">
+						<div className="sidebar-sticky">
+							<Accordion defaultActiveKey="datasourceConfig">
+								<Card>
+									<Accordion.Toggle as={Card.Header} eventKey="datasourceConfig">
+										Datasource
+									</Accordion.Toggle>
+									<Accordion.Collapse eventKey="datasourceConfig">
+										<Card.Body>
+											<DataSourceConfig />
+										</Card.Body>
+									</Accordion.Collapse>
+								</Card>
+							</Accordion>
+						</div>
+					</Col>
+					<Col md="10">
+						<TableDataFilters />
+					</Col>
+				</Row>
+			</Container>
+		</Provider>);
 }
 
 export default App;
