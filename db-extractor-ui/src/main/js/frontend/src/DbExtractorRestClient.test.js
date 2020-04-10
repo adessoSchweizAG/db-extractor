9a4e3b5e642f7077e64bb032c793fab908193913
Daniel Mast <Daniel.Mast@adesso.ch>
2020-04-10 21:17:39 +0200

    react-ui: allow run as war in tomcat

--- db-extractor-ui/src/main/js/frontend/src/DbExtractorRestClient.test.js
+++ db-extractor-ui/src/main/js/frontend/src/DbExtractorRestClient.test.js
@@ -2,6 +2,8 @@ import { enableFetchMocks } from 'jest-fetch-mock';
 
 import DbExtractorRestClient from './DbExtractorRestClient';
 
+window.baseUrl = 'http://localhost/';
+
 enableFetchMocks();
 
 beforeEach(() => {
@@ -9,9 +11,30 @@ beforeEach(() => {
 })
 
 test('get driverClassNames', () => {
-	fetchMock.mockResponseOnce(JSON.stringify(["org.hsqldb.jdbc.JDBCDriver","org.postgresql.Driver"]));
+	const expectedValue = ["org.hsqldb.jdbc.JDBCDriver", "org.postgresql.Driver"];
+	fetchMock.mockResponseOnce(JSON.stringify(expectedValue));
 
 	return DbExtractorRestClient.fetchDriverClassNames().then(result => {
-		expect(result).toEqual(['org.hsqldb.jdbc.JDBCDriver', 'org.postgresql.Driver']);
+		expect(result).toEqual(expectedValue);
+	});
+})
+
+test('datasourceConfig test - success', () => {
+	const expectedValue = { "success": true };
+	fetchMock.mockResponseOnce(JSON.stringify(expectedValue));
+
+	const dataSourceConfig = { id: 0, name: "test", driverClassName: "org.hsqldb.jdbc.JDBCDriver", url: "jdbc:hqsldb:mem:memdb", username: "sa", password: "sa" };
+	return DbExtractorRestClient.dataSourceConfigTest(dataSourceConfig).then(result => {
+		expect(result).toEqual(expectedValue);
+	});
+})
+
+test('datasourceConfig test - error', () => {
+	const expectedValue = { "success": false, "message": "No suitable driver found for " };
+	fetchMock.mockResponseOnce(JSON.stringify(expectedValue));
+
+	const dataSourceConfig = { id: 0, name: "test", driverClassName: "org.hsqldb.jdbc.JDBCDriver", url: "jdbc:hqsldb:mem:memdb", username: "sa", password: "sa" };
+	return DbExtractorRestClient.dataSourceConfigTest(dataSourceConfig).then(result => {
+		expect(result).toEqual(expectedValue);
 	});
 })
